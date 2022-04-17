fe27e6ea55ac16218d190c58a5d5c0effd3b5ca0
Dylan Losada <dylanlosada67@gmail.com>
2022-04-17 20:31:02 -0300

    Subo Modulo Consola

---
+++ Modulo-Consola/src/consola.c
@@ -0,0 +1,116 @@
+#include "consola.h"
+
+int main(int argc, char** argv)
+{
+	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
+
+	int conexion;
+	char* ip;
+	char* puerto;
+	t_paquete* valor;
+
+	t_log* logger;
+	t_config* config;
+
+	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
+	config = iniciar_config();
+
+	// Usando el config creado previamente, leemos los valores del config y los 
+	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	puerto = config_get_string_value(config,"PUERTO_KERNEL");
+	ip = config_get_string_value(config,"IP_KERNEL");
+
+	// Armamos y enviamos el paquete (depuramos)
+	t_paquete* paquete = makePaquete(conexion, argv[2], atoi(argv[1]));
+
+	// Creamos una conexión hacia el servidor
+	conexion = crear_conexion(ip, puerto);
+	enviar_paquete(paquete, conexion);
+	eliminar_paquete(paquete);
+
+	terminar_programa(conexion, logger, config);
+}
+
+t_log* iniciar_logger(void)
+{
+	char* fileLogName = "cliente.log";
+	char* programName = "Cliente Trace";
+	t_log_level LEVEL_ENUM = LOG_LEVEL_TRACE;
+	t_log* nuevo_logger = log_create(fileLogName, programName, 1, LEVEL_ENUM);
+
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+	t_config* nuevo_config = config_create("consola.config");
+
+	return nuevo_config;
+}
+
+/*void leer_consola(t_log* logger)
+{
+	char* leido;
+
+	// La primera te la dejo de yapa
+	leido = readline("> ");
+
+	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+	while(strcmp(leido, "") != 0){
+			log_info(logger, leido);
+			free(leido);
+			leido = readline("> ");
+		}
+
+	// ¡No te olvides de liberar las lineas antes de regresar!
+	free(leido);
+}*/
+
+t_paquete* makePaquete(int conexion, char* pathFile, int processSize)
+{
+	char* intructs = NULL;
+
+	_generateIntructs(pathFile, intructs);
+
+	// Ahora toca lo divertido!
+	t_paquete* paquete = crear_paquete(processSize);
+	agregar_a_paquete(paquete, intructs, sizeof(intructs));
+	free(intructs);
+	return paquete;
+}
+
+void _generateIntructs(char* pathFile, char* intructs){
+	FILE* pseudocodeFile = fopen(pathFile, "r+b");
+
+	while(!feof(pseudocodeFile)){
+		char* intructrRead  = NULL;
+		fread(intructrRead, sizeof(char), 1, pseudocodeFile);
+		strtok(intructrRead,"\n");   //con esto borro el \n que se lee
+		_checkCodeOperatition(intructrRead, intructs);
+	}
+
+	fclose(pseudocodeFile);
+}
+
+void _checkCodeOperatition(char* intructrRead,char* intructs){
+	char* opCode = strtok(intructrRead, " ");
+	const char* NO_OP = "NO_OP";
+
+	if(strcmp(opCode, NO_OP)){
+	  opCode = strtok(NULL, " ");
+	  for(int repeatIntruct = 1; repeatIntruct <= atoi(opCode); repeatIntruct++){
+		  string_append_with_format(intructs, "|", NO_OP);
+	  }
+	}else{
+		string_append_with_format(intructs, "|", intructrRead);
+	}
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
+	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	close(conexion);
+	log_destroy(logger);
+	config_destroy(config);
+}
---
+++ Modulo-Consola/src/utils.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<commons/log.h>
+
+typedef enum
+{
+	NO_OP
+}op_code;
+
+typedef struct
+{
+	int size;
+	void* stream;
+} t_buffer;
+
+typedef struct
+{
+	int processSize;
+	t_buffer* buffer;
+} t_paquete;
+
+void* serializar_paquete(t_paquete* paquete, int bytes)
+{
+	void * magic = malloc(bytes);
+	int desplazamiento = 0;
+
+	memcpy(magic + desplazamiento, &(paquete->buffer->size), sizeof(int));
+	desplazamiento+= sizeof(int);
+	memcpy(magic + desplazamiento, paquete->buffer->stream, paquete->buffer->size);
+	desplazamiento+= paquete->buffer->size;
+
+	return magic;
+}
+
+int crear_conexion(char *ip, char* puerto)
+{
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(ip, puerto, &hints, &server_info);
+
+	// Ahora vamos a crear el socket.
+	int socket_cliente = socket(server_info->ai_family,
+            server_info->ai_socktype,
+            server_info->ai_protocol);
+
+
+	// Ahora que tenemos el socket, vamos a conectarlo
+	connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
+
+	freeaddrinfo(server_info);
+
+	return socket_cliente;
+}
+
+
+void crear_buffer(t_paquete* paquete)
+{
+	paquete->buffer = malloc(sizeof(t_buffer));
+	paquete->buffer->size = 0;
+	paquete->buffer->stream = NULL;
+}
+
+t_paquete* crear_super_paquete(int processSize)
+{
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+
+	//descomentar despues de arreglar
+	paquete->processSize = processSize;
+	//crear_buffer(paquete);
+	crear_buffer(paquete);
+	return paquete;
+}
+
+t_paquete* crear_paquete(int processSize)
+{
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->processSize = processSize;
+	crear_buffer(paquete);
+	return paquete;
+}
+
+void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio)
+{
+	paquete->buffer->stream = realloc(paquete->buffer->stream, paquete->buffer->size + tamanio + sizeof(int));
+
+	memcpy(paquete->buffer->stream + paquete->buffer->size, &tamanio, sizeof(int));
+	memcpy(paquete->buffer->stream + paquete->buffer->size + sizeof(int), valor, tamanio);
+
+	paquete->buffer->size += tamanio + sizeof(int);
+}
+
+void enviar_paquete(t_paquete* paquete, int socket_cliente)
+{
+	int bytes = paquete->buffer->size + 2*sizeof(int);
+	void* a_enviar = serializar_paquete(paquete, bytes);
+
+	send(socket_cliente, a_enviar, bytes, 0);
+
+	free(a_enviar);
+}
+
+void eliminar_paquete(t_paquete* paquete)
+{
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+}
+
+void liberar_conexion(int socket_cliente)
+{
+	close(socket_cliente);
+}
